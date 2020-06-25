a555beaddb86c245a3a89f6c8796d4493dd0fefb
JolteonBot <17845632+ThyImortalGamer@users.noreply.github.com>
2020-06-25 18:10:35 -0400

    Fixed a typo (oridinal -> ordinal)

--- DataTypes.py
+++ DataTypes.py
@@ -1,44 +1,73 @@
 import struct
 import bitarray
 
+
 class DataType:
     pattern = ""
-    def __init__(self,value):
+
+    def __init__(self, value):
         self.value = value
-    def setValue(self,value):
+
+    def setValue(self, value):
         self.value = value
+
     def pack(self):
         return struct.pack(f">{self.pattern}", self.value)
+
     def unpack(self, value):
         self.value = struct.unpack(f">{self.pattern}", value)
         return self.value
 
+
 class Boolean(DataType):
     def pack(self):
         return b"\x01" if self.value else b"\x00"
+
     def unpack(self, value):
         self.value = b"\x01" == value
         return self.value
+
+
 class Byte(DataType):
     pattern = "b"
+
+
 class UnsingedByte(DataType):
     pattern = "B"
+
+
 class Short(DataType):
     pattern = "h"
+
+
 class UnsingedShort(DataType):
     pattern = "H"
+
+
 class Int(DataType):
     pattern = "i"
+
+
 class UnsingedInt(DataType):
     pattern = "I"
+
+
 class Long(DataType):
     pattern = "l"
+
+
 class UnsingedLong(DataType):
     pattern = "L"
+
+
 class Float(DataType):
     pattern = "f"
+
+
 class Double(DataType):
     pattern = "d"
+
+
 class VarInt(DataType):
     def pack(self):
         data = self.value
@@ -52,9 +81,10 @@ class VarInt(DataType):
 
             if data == 0:
                 break
-        if len(oridinal) > 5:
+        if len(ordinal) > 5:
             raise ValueError(f"{self.value} is out of the range of a VarInt")
-        return ordinal       
+        return ordinal
+
     def unpack(self, value):
         """ Unpack the varint """
         data = 0
@@ -72,7 +102,9 @@ class VarInt(DataType):
 
         self.value = data
         return data
- class VarLong(DataType):
+
+
+class VarLong(DataType):
     def pack(self):
         data = self.value
         """ Pack the var int """
@@ -87,7 +119,8 @@ class VarInt(DataType):
                 break
         if len(oridinal) > 7:
             raise ValueError(f"{self.value} is out of the range of a VarLong")
-        return ordinal 
+        return ordinal
+
     def unpack(self, value):
         """ Unpack the varint """
         data = 0
@@ -104,13 +137,17 @@ class VarInt(DataType):
                 break
         self.value = data
         return data
+
+
 class String(DataType):
     def pack(self):
         byte = self.value.encode("utf-8")
         return VarInt(len(byte)).pack + byte
+
+
 class Chat(String):
     pass
+
+
 class Identifier(String):
     pass
-
-              
